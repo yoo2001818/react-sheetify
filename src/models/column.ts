d97f1c6d6c100b3586456416c33189af91e3d916
Duknam Yoo <yoo2001818@gmail.com>
2020-04-23 23:50:18 +0900

    Add base column methods

--- src/models/column.ts
+++ src/models/column.ts
@@ -1,8 +1,24 @@
 export interface ColumnImpl {
   // TODO Various logic here... This will be implemented in
   // 'col' component, which is delegated by column class
+  focus(): void,
 }
 
 export class Column {
   
+  reportFocus(): void {
+
+  }
+  
+  reportBlur(): void {
+
+  }
+
+  setImpl(impl: ColumnImpl): void {
+
+  }
+
+  focus(): void {
+
+  }
 }
